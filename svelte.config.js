ba9b0746d9e1333ea0ec8e2fdc25062489999330
Matt Kilcup <nevermindthelabel@gmail.com>
2023-02-18 12:20:16 -0700

    updated sveltekit and other dependencies

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,7 @@
 import adapter from '@sveltejs/adapter-static';
 import { mdsvex } from 'mdsvex';
-import preprocess from 'svelte-preprocess';
+// import preprocess from 'svelte-preprocess';
+import {vitePreprocess} from '@sveltejs/kit/vite'
 import rehypeAutolinkHeadings from 'rehype-autolink-headings';
 import rehypeSlug from 'rehype-slug';
 
@@ -10,7 +11,8 @@ const config = {
 	extensions: ['.svelte', '.md'],
 
 	preprocess: [
-		preprocess({ postcss: true }),
+		// preprocess({ postcss: true }),
+		vitePreprocess({ postcss: true }),
 		mdsvex({
 			extensions: ['.md'],
 			rehypePlugins: [rehypeSlug, rehypeAutolinkHeadings]
