1bcdeed88adee887383a8f96515eeaa010b58d83
Matt Kilcup <nevermindthelabel@gmail.com>
2023-02-20 12:56:32 -0700

    updated routes

--- src/lib/utils/posts.ts
+++ src/lib/utils/posts.ts
@@ -8,3 +8,15 @@ export const fetchThePosts = async () => {
 	);
 	return { thePosts };
 };
+
+export const fetchAPost = async () => {
+	const thePosts = await Promise.all(
+		Object.entries(import.meta.glob('/src/lib/posts/*.md')).map(async ([path, resolve]) => {
+			const { metadata } = await resolve();
+			const slug = path.split('/').pop()?.slice(0, -3);
+			return { ...metadata, slug };
+		})
+	);
+	console.log(thePosts);
+	return { thePosts };
+};
--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -4,12 +4,16 @@
 	export let data: PageData;
 </script>
 
-{#each data.thePosts as post}
-	<Card>
-		<h2>{post.title}</h2>
-		<p>{post.excerpt}</p>
-	</Card>
-{/each}
+<div class="content">
+	{#each data.thePosts as post}
+		<Card>
+			<a href="/blog/{post.slug}">
+				<h2>{post.title}</h2>
+				<p>{post.excerpt}</p>
+			</a>
+		</Card>
+	{/each}
+</div>
 
 <style lang="postcss">
 </style>
