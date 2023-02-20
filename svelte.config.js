6f452f7327c4e422fc998e2020f15e1104f0e714
Matt Kilcup <nevermindthelabel@gmail.com>
2023-02-20 11:58:43 -0700

    updated config

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,6 @@
 import adapter from '@sveltejs/adapter-static';
 import { mdsvex } from 'mdsvex';
-import {vitePreprocess} from '@sveltejs/kit/vite'
+import { vitePreprocess } from '@sveltejs/kit/vite';
 import rehypeAutolinkHeadings from 'rehype-autolink-headings';
 import rehypeSlug from 'rehype-slug';
 
@@ -10,7 +10,6 @@ const config = {
 	extensions: ['.svelte', '.md'],
 
 	preprocess: [
-		// preprocess({ postcss: true }),
 		vitePreprocess({ postcss: true }),
 		mdsvex({
 			extensions: ['.md'],
