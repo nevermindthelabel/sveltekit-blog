61e2fc3c17fedad89dca02d95f07f2a7a1f6b4a9
Matt Kilcup <nevermindthelabel@gmail.com>
2023-02-21 20:46:03 -0700

    reinstalled mdsvex

---
+++ mdsvex.config.js
@@ -0,0 +1,16 @@
+import { defineMDSveXConfig as defineConfig } from 'mdsvex';
+import rehypeAutolinkHeadings from 'rehype-autolink-headings';
+import rehypeSlug from 'rehype-slug';
+
+const config = defineConfig({
+	extensions: ['.svelte.md', '.md', '.svx'],
+
+	smartypants: {
+		dashes: 'oldschool'
+	},
+
+	remarkPlugins: [],
+	rehypePlugins: [rehypeAutolinkHeadings(), rehypeSlug()]
+});
+
+export default config;
