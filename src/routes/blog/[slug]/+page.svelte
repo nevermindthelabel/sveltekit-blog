80ab3442dbed43f636e3477cf41b9dd9705c324a
Matt Kilcup <nevermindthelabel@gmail.com>
2023-02-21 20:48:17 -0700

    new process loading md with mdsvex

--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -2,6 +2,12 @@
 	import type { PageData } from './$types';
 
 	export let data: PageData;
+	console.log(data);
+	type C = $$Generic<typeof SvelteComponentTyped<any, any, any>>;
+	$: component = data.component as unknown as C;
 </script>
 
-meme
+<div class="content">
+	<h1>thing</h1>
+	<svelte:component this={component} />
+</div>
